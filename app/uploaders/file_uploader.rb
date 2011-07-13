6d872ef124196946e8685cb04e2913540a6534b7
seji <seji.ru@gmail.com>
2011-07-13 05:00:52 +0000

    do not use rackspace

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -3,7 +3,7 @@
 class FileUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
-  storage :fog
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   def store_dir
